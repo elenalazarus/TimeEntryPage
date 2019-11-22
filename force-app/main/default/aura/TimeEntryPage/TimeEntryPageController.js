93cbfef6f92f72b2ff49d0a591a0e7333cb5fe1a
lena <olazarenko@corevalue.net>
2019-11-22 16:23:51 +0200

    Picklist updated, added columns with days of weeks

--- force-app/main/default/aura/TimeEntryPage/TimeEntryPageController.js
+++ force-app/main/default/aura/TimeEntryPage/TimeEntryPageController.js
@@ -1,12 +1,11 @@
 ({
     doInit : function(component, event, helper) {
         helper.doInit(component);
-        helper.getConsultants(component);
+        
         
     },
     
-    onPicklistChange: function(component, event, helper) {
-        // get the value of select option
-        alert(event.getSource().get("v.value"));
+    onchange: function(component, event, helper) {
+        helper.getNeededTasks(component);
     },
 })
--- force-app/main/default/aura/TimeEntryPage/TimeEntryPageHelper.js
+++ force-app/main/default/aura/TimeEntryPage/TimeEntryPageHelper.js
@@ -1,23 +1,39 @@
 ({
     doInit : function(component) {
-        var recID = component.get("v.recordId");
+
         var action = component.get("c.getTasks");
+        
+       	action.setCallback(this, function(response){
+            
+            var data = response.getReturnValue();
+            console.log(data);
+            component.set("v.taskList", data);
+        });
+       
+
+        $A.enqueueAction(action);
+
+    },
+    
+    getNeededTasks: function(component){
+        var action = component.get("c.getNeededTasks");
         action.setParams({
             recordId: recID
         });
+        
         action.setCallback(this, function(response){
+            
             var data = response.getReturnValue();
-            component.set("v.taskList", data);
+            console.log(data);
+            console.log("Hello");
+            var options = []; 
+		        
+            for(var i = 0; i < data.length; i++){
+                options.push({selected: false, label: data[i].Consultant__r.Name, value: data[i].Consultant__c});
+            } 
+        	component.set("v.options", options);
+        	$A.enqueueAction(action);
         });
-        $A.enqueueAction(action);
-    },
-    getConsultants : function(component) {
-        var action = component.get("c.getConsultants");
-        action.setCallback(this, function(response){
-            var data = response.getReturnValue();
-            component.set("v.consultants", data);
-        });
-        $A.enqueueAction(action);
-    },
+	},
         
 })
